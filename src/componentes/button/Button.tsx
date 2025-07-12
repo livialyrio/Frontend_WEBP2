83a4476a474d40344d199a51a39ffef5cef2dba4
DevCarlosAndrade <carlos.ed.andrade97@gmail.com>
2025-07-12 16:30:37 -0300

    Layout tela de Medicamentos disponiveis

--- src/componentes/button/Button.tsx
+++ src/componentes/button/Button.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-export default function Button() {
+interface ButtonProps {
+  label: string;
+  onClick?: () => void;
+}
+
+export default function Button({ label, onClick }: ButtonProps) {
   return (
     <button className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded"> 
       Enviar 
